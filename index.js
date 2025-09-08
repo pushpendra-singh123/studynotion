b47dfab9b37ac297dd60a02d6abfcb5fd4211677
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-09-08 16:11:59 +0530

    connect to mongodb

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const database = require("./config/database");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+//database connect
+database.connect();
+//middlewares
+app.use(express.json());
+//def route
+
+app.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: "Your server is up and running....",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`App is running at ${PORT}`);
+});
