3a70359fc735acad425543a09830aaf8af607ed5
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-09-10 17:31:54 +0530

    Implement login & signup with JWT, manage session using cookies, and send OTP via email for verification

--- models/OTP.js
+++ models/OTP.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-
+const mailSender = require("../utils/mailSender");
+const emailTemplate = require("../mail/templates/emailVerificationTemplate");
 const OTPSchema = new mongoose.Schema({
   email: {
     type: String,
@@ -12,7 +13,7 @@ const OTPSchema = new mongoose.Schema({
   createdAt: {
     type: Date,
     default: Date.now,
-    expires: 60 * 5, // The document will be automatically deleted after 5 minutes of its creation time
+    expires: 60 * 5,
   },
 });
 
