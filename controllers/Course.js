84c475cafda49c68434635fce2eca52a9722f6da
pushpendra-singh123 <pushpendra.singh4_cs22@gla.ac.in>
2025-09-10 22:56:45 +0530

    create handler to get all courses list

--- controllers/Course.js
+++ controllers/Course.js
@@ -127,3 +127,34 @@ exports.createCourse = async (req, res) => {
     });
   }
 };
+
+// Get Course List
+exports.getAllCourses = async (req, res) => {
+  try {
+    const allCourses = await Course.find(
+      { status: "Published" },
+      {
+        courseName: true,
+        price: true,
+        thumbnail: true,
+        instructor: true,
+        ratingAndReviews: true,
+        studentsEnrolled: true,
+      }
+    )
+      .populate("instructor")
+      .exec();
+
+    return res.status(200).json({
+      success: true,
+      data: allCourses,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(404).json({
+      success: false,
+      message: `Can't Fetch Course Data`,
+      error: error.message,
+    });
+  }
+};
